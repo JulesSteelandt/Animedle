44be99f0a9e9f69e87610215a4a225e48cebccda
Sonii08 <sonij590833@gmail.com>
2023-09-22 11:40:28 +0200

    feat: setup api

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
 function App() {
 
-  const test = fetch('https://api.jikan.moe/v4/anime/43608')
-.then(response => {
-return response.json();
-})
-    .catch(error => {
- console.log(error);
-});
 
-  test.then((data) => {
-      console.log(data);
-    }
-  );
 
   return (
     <p> start </p>
---
+++ src/utils/appConsts.tsx
@@ -0,0 +1,10 @@
+export const API_URL = 'https://api.jikan.moe/v4';
+
+export const API_ANIME = '/anime';
+
+export const API_MANGA = '/manga';
+
+export const API_TOP = '/top';
+
+export const API_CHARACTER = '/character';
+
